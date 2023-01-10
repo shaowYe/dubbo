d4d2b2b1ba5bed647a291fc33e9d03b3f30ad837
shaowei ye <yesw@broada.com>
2023-01-10 19:25:38 +0800

    更新zk注册中心 DelegateZookeeperRegistryFactory

--- dubbo-uyun/dubbo-registry-zookeeper-delegate/src/main/java/uyun/whale/dubbo/registry/DelegateZookeeperRegistryFactory.java
+++ dubbo-uyun/dubbo-registry-zookeeper-delegate/src/main/java/uyun/whale/dubbo/registry/DelegateZookeeperRegistryFactory.java
@@ -1,6 +1,7 @@
 package uyun.whale.dubbo.registry;
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.Inject;
 import org.apache.dubbo.registry.Registry;
 import org.apache.dubbo.registry.support.AbstractRegistryFactory;
 import org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter;
@@ -9,33 +10,38 @@ import org.slf4j.LoggerFactory;
 
 /**
  * DelegateZookeeperRegistryFactory.
- * 
+ *
  * @author hesy
  */
 public class DelegateZookeeperRegistryFactory extends AbstractRegistryFactory {
-	private static final Logger logger = LoggerFactory.getLogger(DelegateZookeeperRegistryFactory.class);
-	private ZookeeperTransporter zookeeperTransporter;
+    private static final Logger logger = LoggerFactory.getLogger(DelegateZookeeperRegistryFactory.class);
+    private ZookeeperTransporter zookeeperTransporter;
 
-	public void setZookeeperTransporter(ZookeeperTransporter zookeeperTransporter) {
-		this.zookeeperTransporter = zookeeperTransporter;
-	}
+    @Inject(enable = false)
+    public void setZookeeperTransporter(ZookeeperTransporter zookeeperTransporter) {
+        this.zookeeperTransporter = zookeeperTransporter;
+    }
 
-	@Override
-	public Registry createRegistry(URL url) {
-		String delegateZkUrl = url.getParameter("delegate");
-		URL parentZKUrl = null;
-		if (delegateZkUrl != null) {
-			url = url.removeParameter("delegate");
-			String[] hostPorts = delegateZkUrl.split(":");
-			parentZKUrl = createDelegateURL(url, hostPorts[0], hostPorts[1]);
-			logger.info("using delegate : {}", parentZKUrl);
-		}
-		return new DelegateZookeeperRegistry(url, parentZKUrl, zookeeperTransporter);
-	}
+    public DelegateZookeeperRegistryFactory(ZookeeperTransporter zookeeperTransporter) {
+        this.zookeeperTransporter = zookeeperTransporter;
+    }
 
-	private URL createDelegateURL(URL url, String host, String port) {
-		String _host = host != null ? host : url.getHost();
-		int _port = port != null ? Integer.parseInt(port) : url.getPort();
-		return new URL(url.getProtocol(), url.getUsername(), url.getPassword(), _host, _port, url.getPath(), url.getParameters());
-	}
+    @Override
+    public Registry createRegistry(URL url) {
+        String delegateZkUrl = url.getParameter("delegate");
+        URL parentZKUrl = null;
+        if (delegateZkUrl != null) {
+            url = url.removeParameter("delegate");
+            String[] hostPorts = delegateZkUrl.split(":");
+            parentZKUrl = createDelegateURL(url, hostPorts[0], hostPorts[1]);
+            logger.info("using delegate : {}", parentZKUrl);
+        }
+        return new DelegateZookeeperRegistry(url, parentZKUrl, zookeeperTransporter);
+    }
+
+    private URL createDelegateURL(URL url, String host, String port) {
+        String _host = host != null ? host : url.getHost();
+        int _port = port != null ? Integer.parseInt(port) : url.getPort();
+        return new URL(url.getProtocol(), url.getUsername(), url.getPassword(), _host, _port, url.getPath(), url.getParameters());
+    }
 }
